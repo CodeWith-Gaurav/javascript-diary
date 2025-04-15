e21d1ef2a8e903380e82c74b3430f48a5c343fc4
code-withGV <gauravvashistha452@gmail.com>
2025-04-15 23:27:17 +0530

    Day_2_code

---
+++ Day_2/script.js
@@ -0,0 +1,60 @@
+let str = "apple";
+let ans = str.slice(0, 4); //slice value from 0 to (4-1)=3-->appl
+console.log(ans)
+
+// `` -- template literals/template strings
+//Template Strings allow multiline strings
+//Template String provide an easy way to interpolate variables and expressions into strings.
+
+let str1 = `hey my age is ${12 + 2}`;
+console.log(str1)
+// The method is called string interpolation.
+let firstName = "John";
+let lastName = "Doe";
+
+let text = `Welcome ${firstName}, ${lastName}!`;
+console.log(text)
+
+//split
+let str2 = "Hello ji kaise hooo!!"
+console.log(str2.split('ai'));
+
+//replace
+let str3 = "Hello Ji kaise ho";
+//original string change nhi hogi 
+//ek nyi copy of string bnegi usme replace hoga and pehle occurence ko change krega bs
+let ans2 = str3.replace('i', 'y');
+console.log(ans2);
+console.log(str3)
+let ans3 = str3.replaceAll('i', 'y');
+console.log(ans3);
+
+//includes
+let str4 = "Hello, World!"
+console.log(str4.includes('Hell'));
+
+//switch
+// switch(value){
+//     case value:
+//         //code
+//         break;
+//     case value:
+//         //code
+//         break;
+//     default:
+//         //code
+// }
+switch (3) {
+    case 1:
+        console.log('One');
+        break;
+    case 2:
+        console.log('Two');
+        break;
+    case 3:
+        console.log('three');
+        break;
+    default:
+        console.log('Default');
+}
+
