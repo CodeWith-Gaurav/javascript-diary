f1b6eb616ad02de34034fe2f4f37c7f5b6364e78
code-withGV <gauravvashistha654@gmail.com>
2025-05-09 01:19:08 +0530

    Js_practice_started

---
+++ JS_Practice_1/script.js
@@ -0,0 +1,11 @@
+//Problem-1. Log "Hello, JavaScript!" to the console in 3 different ways.
+console.log("Hello World")
+console.warn("Hello World")
+console.error("Hello World")
+console.info("Hello World")
+console.table({name: 'harsh', age: 25})
+
+//Problem-2. Perform 35 * 2 - (10 / 2) + 7 and log the result.
+console.log(35 * 2 - (10 / 2) + 7);
+
+//Problem-
